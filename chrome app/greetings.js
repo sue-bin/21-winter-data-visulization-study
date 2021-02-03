1cf0f9a4581e00b78672fe0fe17060ae04eeaae7
Suebin Kim <sue_bin1029@yonsei.ac.kr>
2021-02-04 01:29:54 +0900

    Complete making todo list

--- chrome app/greetings.js
+++ chrome app/greetings.js
@@ -5,8 +5,20 @@ const form = document.querySelector(".js-form"),
 const USER_LS= "currentUser",
 SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text)
+}
+
+function handleSubmit(){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
 function askForName(){
     form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text){
@@ -29,8 +41,6 @@ function loadName(){
 
 function init(){
     loadName();
-
-
 }
 
 init();
